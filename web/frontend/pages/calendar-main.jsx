a17bf943bdd02a06e4142d593acdb59c6edd0c3f
nuc <nicholas.pippo@gmail.com>
2023-06-15 13:32:52 -0700

    test

---
+++ web/frontend/pages/calendar-main.jsx
@@ -0,0 +1,32 @@
+/*Author: Nicolas Pippo*/
+/* This file is for the main calendar view. It will only draw the calendar meaning displaying 
+the current month, the days and allowing for going to the next month. This uses the preexisting 
+react-calendar which is licensed under MIT license*/
+
+import {
+    Card,
+    EmptyState,
+    Layout,
+    Page,
+    SkeletonBodyText,
+  } from "@shopify/polaris";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+//import 'react-calendar/dist/Calendar.css';
+import './calendar.css'
+
+export default function calendarView()
+{
+   const [value,onChange] = useState(new Date());
+    
+   return( 
+    <Page>
+
+    {/* <Card sectioned> */}
+           <Calendar onChange={onChange} value={value}/>
+    {/* </Card> */}
+    </Page>
+    );
+    
+    
+}
