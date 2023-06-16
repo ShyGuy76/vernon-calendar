a56fa126fda1dfa45e86c01f47fc0e782967c542
nuc <nicholas.pippo@gmail.com>
2023-06-15 18:31:54 -0700

    bad repo?

--- web/frontend/pages/calendar-main.jsx
+++ web/frontend/pages/calendar-main.jsx
@@ -2,7 +2,9 @@
 /* This file is for the main calendar view. It will only draw the calendar meaning displaying 
 the current month, the days and allowing for going to the next month. This uses the preexisting 
 react-calendar which is licensed under MIT license*/
+/*-------------------- */
 
+/*need to test the git repo because the vernon calendar is a repo in a repo */ 
 import {
     Card,
     EmptyState,
